ceb45a7f381d51bdb78d470231b82d1f32ffb345
William Lundkvist <william.lundqvist02@gmail.com>
2023-11-09 16:47:30 +0100

    Ändrar bild

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -23,12 +24,13 @@ public class MainActivity extends AppCompatActivity {
                 TextView windspeed = findViewById(R.id.WindSpeed);
                 TextView cloudiness = findViewById(R.id.Cloudiness);
                 TextView precipitation = findViewById(R.id.Precipitation);
+                ImageView currentWeather= findViewById(R.id.current_weather);
 
                 temperature.setText("Temperature: " + "Variabel");
                 windspeed.setText("WindSpeed: " + "Variabel");
                 cloudiness.setText("Cloudiness: " + "Variabel");
                 precipitation.setText("Precipitation: Between " + "Variable 1" + " and " + "Variabel 2");
-
+                currentWeather.setImageResource(R.drawable.ic_launcher_foreground);
             }
         });
     }
